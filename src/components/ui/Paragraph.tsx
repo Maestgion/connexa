33663051036860cbaec409b6f4aad5101046f16e
Abhishek Dubey <7abhishek1410@gmail.com>
2023-08-09 01:54:16 +0530

    refactor: Paragraph component

---
+++ src/components/ui/Paragraph.tsx
@@ -0,0 +1,33 @@
+import { FC, HTMLAttributes, forwardRef } from 'react';
+import { cva, VariantProps } from "class-variance-authority"
+import { cn } from '@/lib/utils';
+
+const paragraphVariants = cva(
+    "text-white dark:text-black max-w-prose text-center mb-4", {
+        variants: {
+           size:{
+            default: "text-lg sm:text-md",
+            sm: "text-sm, sm:text-base"
+           }
+        },
+        defaultVariants:{
+            size: 'default',
+        }
+    }
+)
+
+interface ParagraphProps extends HTMLAttributes<HTMLParagraphElement>, VariantProps<typeof paragraphVariants>{
+    
+}
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps >(({className, size, children, ...props}, ref)=>{
+    return(
+        <p ref={ref} {...props} className={cn(paragraphVariants({size, className}))}>
+            {children}
+        </p>
+    )
+})
+
+Paragraph.displayName = "Paragraph"
+
+export default Paragraph
